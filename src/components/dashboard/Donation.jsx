88e578e17ee9dbe6c8906f93c16329ee58a4a3a8
Pateldhruv2323 <130552819+Pateldhruv2323@users.noreply.github.com>
2025-08-06 12:05:36 +0530

    changes

--- src/components/dashboard/Donation.jsx
+++ src/components/dashboard/Donation.jsx
@@ -18,7 +18,7 @@ function Donation() {
         return;
       }
 
-      const res = await axios.get(`/api/donations`, {
+      const res = await axios.get(`http://localhost:5000/api/donations`, {
         params: { username }
       });
 
