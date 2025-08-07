f75b4162ea68618cdf196b25f420b66baaf6a5e3
Pateldhruv2323 <130552819+Pateldhruv2323@users.noreply.github.com>
2025-08-07 15:50:28 +0530

    chnages

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -4,7 +4,7 @@ import { Plus, Home, BarChart2, Search, Users, DollarSign, LogOut, X, Menu, Down
 import { motion, AnimatePresence } from "framer-motion";
 import QuizOverlay from "../components/dashboard/QuizOverlay";
 import Joyride, { CallBackProps, STATUS, Step}  from 'react-joyride';
-import Tour from 'reactour';
+import { Tour } from '@reactour/tour';
 
 
 
