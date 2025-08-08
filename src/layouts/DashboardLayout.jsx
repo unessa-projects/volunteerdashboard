33a7bdae1bd4ce7273f024175cd36d672e9e5a5f
Pateldhruv2323 <130552819+Pateldhruv2323@users.noreply.github.com>
2025-08-08 12:47:10 +0530

    Update DashboardLayout.jsx

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -16,10 +16,12 @@ const [user, setUser] = useState(null);
 // Username and avatar (will be updated after load)
 const username = user?.name ? user.name.split(" ")[0] : "User";
 const avatar = user?.avatar || null;
+   console.log(user.name);
 
 // Rehydrate user from localStorage when the component mounts
 useEffect(() => {
   const storedUser = localStorage.getItem("googleUser");
+  
   if (storedUser) {
     setUser(JSON.parse(storedUser));
   } else {
