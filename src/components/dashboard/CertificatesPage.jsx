210bb2b53c8dae49fae81f7245743ed03c43492e
Yash Patel <167839006+yashpatel219@users.noreply.github.com>
2025-08-19 11:55:39 +0530

    Update CertificatesPage.jsx

--- src/components/dashboard/CertificatesPage.jsx
+++ src/components/dashboard/CertificatesPage.jsx
@@ -15,7 +15,7 @@ const CertificatesPage = () => {
 
   useEffect(() => {
     if (user?.email) {
-      axios.get(`https://unessa-backend.onrender.com/api/users/quiz-status/${user.email}`)
+      axios.get(`https://internapi.unessafoundation.org/api/users/quiz-status/${user.email}`)
         .then(res => setQuizStatus(res.data.quizStatus))
         .catch(() => setQuizStatus("notAttempted"));
     }
