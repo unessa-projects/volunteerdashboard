d2c671cd6d0711477d48d5b528845c292b6267bb
Pateldhruv2323 <130552819+Pateldhruv2323@users.noreply.github.com>
2025-08-20 11:59:46 +0530

    changes

--- src/components/FormFields.jsx
+++ src/components/FormFields.jsx
@@ -242,15 +242,19 @@ const FormFields = ({ formData, onChange, submitted }) => {
             <FaIdCard className="absolute right-2 top-2 text-gray-400" />
           </div>
           <div className="relative">
-            <input
-              type="date"
-              placeholder="Date of Birth"
-              value={formData.dob || ""}
-              onChange={(e) => onChange("dob", e.target.value)}
-              className="w-full border-b py-2 pr-8 outline-none border-gray-300"
-            />
-            <FaBirthdayCake className="absolute right-2 top-2 text-gray-400" />
-          </div>
+  <input
+    type="text"
+    placeholder="Date of Birth"
+    value={formData.dob || ""}
+    onFocus={(e) => (e.target.type = "date")}
+    onBlur={(e) => {
+      if (!formData.dob) e.target.type = "text";
+    }}
+    onChange={(e) => onChange("dob", e.target.value)}
+    className="w-full border-b py-2 pr-8 outline-none border-gray-300"
+  />
+  <FaBirthdayCake className="absolute right-2 top-2 text-gray-400" />
+</div>
         </div>
       )}
     </div>
